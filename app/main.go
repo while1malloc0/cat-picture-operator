9d216d7fbdfef50c1fe4f47e722ea44ffdfcaf1e
John Turner <jturner@squarespace.com>
2018-09-20 14:45:29 -0400

    Redo HTML

--- app/main.go
+++ app/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Cat incoming!")
-
-		catResp, err := http.Get("http://thecatapi.com/api/images/get")
-
-		if err != nil {
-			fmt.Fprintf(w, "Could not get cat: %v", err)
-		}
-
-		w.WriteHeader(catResp.StatusCode)
-		io.Copy(w, catResp.Body)
+		w.WriteHeader(200)
+		html := `
+<html>
+<img src="https://api.thecatapi.com/v1/images/search?format=src&size=%s&formats=%s">
+<footer>Powered by <a href="https://thecatapi.com">The Cat API</a></footer>
+</html>
+`
+		s := os.Getenv("CAT_API_SIZE")
+		fs := os.Getenv("CAT_API_FORMATS")
+		fmt.Fprintf(w, fmt.Sprintf(html, s, fs))
 	})
 
 	http.ListenAndServe("0.0.0.0:8080", nil)
